522ad30caf13093c9816f558b9f28cb460fdd2ec
b1o <erikghabuzyan6@gmail.com>
2026-09-10 12:18:40 -0700

    Start visual asset preloading before app render

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './portal-bio.css'
 import './performance.css'
+import './preload'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
